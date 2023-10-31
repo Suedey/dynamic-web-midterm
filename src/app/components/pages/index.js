76cda9955db2d44e1ce4f5cd69926805564cb94e
Suedey <Ot2017@nyu.edu>
2023-10-31 14:45:33 -0400

    Push 3

--- src/app/components/pages/index.js
+++ src/app/components/pages/index.js
@@ -1,9 +1,43 @@
-import "../app/globals.css"
+import "../app/globals.css";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
-export default function Home(){
-    return(
-        <main>
-            <h1>Midterm</h1>
-        </main>
+const App = () => {
+    const [playerName, setPlayerName] = useState([]);
+    const [playerPic, setPlayerPic] = useState([]);
+
+    const fetchData= () => {
+        const playerAPI = 'https://www.balldontlie.io/api/v1/players'
+        const playerPic = 'https://nba-players.herokuapp.com/players/james/lebron'
+
+        const getNBAPlayer = axios.get(playerAPI)
+        const getPlayerPic = axios.get(playerPic)
+        axios.all([getNBAPlayer, getPlayerPic]).then(
+            axios.spread((...allData) => {
+                const allDataPlayer = allData[0].data.first_name
+                const getNBAPlayerPic = alldata[1].config.url;
+
+                setPlayerName(allDataPlayer)
+                setPlayerPic(getNBAPlayerPic)
+                
+            })
+        )
+            
+        }
+
+        useEffect(() => {
+            fetchData()
+        }, [])
+
+
+
+    return (
+        <div className="App">
+            Player Name is: {playerName}
+            <img src={playerPic}/>
+
+        </div>
     );
 }
+
+export default App;
